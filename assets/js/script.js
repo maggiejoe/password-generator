87abe3d94e10447685861fc37493db4d7b6a5cb0
maggiejoe <maggiejoseph94@gmail.com>
2022-03-11 10:00:29 -0500

    if loop started for confirmLength variable, committing for assistance with BCS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,46 @@
-// Assignment code here
-
-// CREATE VARIALBE FOR EACH PASSWORD CRITERIA (LOWERCASE, UPPERCASE, NUMERIC, SPECIAL)
+// Variables representing all the criteria necessary for generating a secure password
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberic = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ",", "<", ".", ">", "/", "?"];
 
+// Variables representing if the user confirming if they do or do not want certain criteria in their password
 var confirmLowercase;
 var confirmUppercase;
 var confirmNumeric;
 var confirmSpecialChar;
 var confirmLength;
 
-function generatePassword() {
+// generate password beginning with asking the user what criteria they would like in their password
+
+var generatePassword = function () {
+  console.log(generatePassword);
   var confirmLowercase = window.confirm("Would you like lower case characters in your password?");
   var confirmUppercase = window.confirm("Would you like upper case characters in your password?");
   var confirmNumeric = window.confirm("Would you like numbers in your password?");
-  var confirmSpecialChar = window.confirm("Would you like special characters in your password?");
-
-  var confirmLength = window.prompt("How long would you like your password? You must choose a number between 8 and 128.")
-    if(confirmLength <= 7 || confirmLength >= 129) {
-      alert("You must choose a length between 8 and 128. Please try again.");
-    }
-    
-}
+  var confirmSpecialChar = window.confirm("Would you like special characters in your password?"); 
+  var confirmLength = window.prompt("How long would you like your password to be? Please choose a length between 8 and 128 characters.");
   
+  if (confirmLowercase = true) { // ??then add to for loop to generate the password??
+    // insert more if statements to put action to the user selection of what criteria they would like for their password
+    };
+
+  var lengthConfirmation = function () {
+  
+    if (confirmLength >= 8 || confirmLength <= 128) {
+      window.alert("Thank you for your submissions! Your password will be shown below!");
+    } else {
+      window.alert("You must choose a length between 8 and 128 characters. Please try again.");
+      while (confirmLength <=7 || confirmLength >=129) {
+        return lengthConfirmation();
+      }
+    };
+  };
+}
+   // (while loop to be able to jump out if possible)need to add a loop that continues to ask this question until a correct number is selected. currently if the user chooses
+    // a character length incorrectly more than once, it does not ask them to try again
 
 
-
-// CREATE IF STATEMENTS
-  // IF USER SELECTS CANCEL(FALSE), THE VARIABLE IS NOT CALLED IN THE PASSWORD GENERATE FUNCTION
-  // IF USER SELECTS OK(TRUE), THE VARIABLE IS CALLED INTO THE PASSWORD GENERATE FUNCTION
-  // THIS HAPPENS ON A LOOP UNTIL ALL 4 QUESTIONS ARE ANSWERED, THEN LOOP BREAKS
-
-// CREATE A VARIABLE THAT ASKS THE USER HOW MANY CHARACTERS THEY WANT IN THEIR PASSWORD USING WINDOW.PROMPT
-  // THE ANSWER IS CALLED INTO A FUNCTION THAT GENERATES THE PASSWORD
-
-// CREATE A FUNCTION THAT GENERATES THE PASSWORD
 
 
 // Get references to the #generate element
@@ -53,25 +57,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-// REMEMBER THE DECK OF CARDS PRACTICE FOR BREAKING DOWN CODE
-
-// CREATE ISSUES IN GITHUB FOR STEP BY STEP OF EACH STEP I'LL TAKE IN CREATING THIS JS CODE
-
-// 1) when button is clicked, ask user what citeria they require for their password
-  //create prompts to ask the user what criteria they would like for their password
-  //create objects for citeria 
-
-// 2) ask user what their required length is between 8 - 128 characters
-  //create prompt that asks the use how many character they would like their password to be
-
-// 3) when critera is given, generate random password using specific criteria
-  //add randomization into the selected criteria
-  //cut the password back to the desired length requested from user
-
-// 4) generate code and present it in the webpage
